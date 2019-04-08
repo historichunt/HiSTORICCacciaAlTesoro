e0c5636d3a4cf16d60e94f19e2e7f08ee5962139
Federico Sangati <federico.sangati@gmail.com>
2019-04-08 15:52:00 +0200

    fixed script for downloading selfies from airtable

--- HistoricTrentoBot/airtable_utils.py
+++ HistoricTrentoBot/airtable_utils.py
@@ -2,11 +2,11 @@
 
 import key
 from airtable import Airtable
-import requests
-import os
 
 def downloadSelfies(table_name, output_dir):
-    table_id = key.GAMES_ID[table_name]['Airtable_Table_ID']
+    import requests
+    import os
+    table_id = key.GAMES_ID[table_name]['Airtable_Risultati_ID']
     GAME_TABLE = Airtable(table_id, 'Games', api_key=key.AIRTABLE_API_KEY)
     table_entries = GAME_TABLE.get_all()
     for entry in table_entries:
@@ -14,9 +14,11 @@ def downloadSelfies(table_name, output_dir):
         group_name = fields['GROUP_NAME']
         output_dir_group = os.path.join(output_dir, group_name)
         os.mkdir(output_dir_group)
+        print('Downloading selfies for group {}'.format(group_name))        
         for selfie in fields['GROUP_SELFIES']:
             url = selfie['url']
             file_name = selfie['filename']
+            print('\t{}'.format(file_name))        
             output_file = os.path.join(output_dir_group, file_name)
             r = requests.get(url)
             with open(output_file, 'wb') as output:
@@ -25,4 +27,4 @@ def downloadSelfies(table_name, output_dir):
 
 if __name__ == "__main__":
     #downloadSelfies('Suffragio_1_July_2018', '/Users/fedja/Downloads/caccia')
-    downloadSelfies('SantaMassenza_16_December_2018', '/Users/fedja/Downloads/Caccia_Santa_Massenza_16_Dicembre')
+    downloadSelfies('05_Aldeno_03_May_2019', '/Users/fedja/Downloads/05_Aldeno_03_May_2019')
