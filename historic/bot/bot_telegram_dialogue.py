c5f0b13c3e543e726bb74568dad51076debcb787
Federico <federico.sangati@gmail.com>
2022-12-16 14:46:14 +0900

    fixed bug in cmd

--- historic/bot/bot_telegram_dialogue.py
+++ historic/bot/bot_telegram_dialogue.py
@@ -1405,7 +1405,7 @@ def deal_with_admin_commands(p, message_obj):
             else:
                 send_message(p, f'User id {user_id} non valido')
             return True
-        if text_input == '/refresh_':
+        if text_input.startswith('/refresh_'):
             user_id = text_input.split('_',1)[1]
             u = Person.get_by_id(user_id)
             if u:
